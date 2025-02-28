fe9a97f77919ab49d7a734876db09d768ec2d46a
PRAMIHA <pramiha16@gmail.com>
2025-02-28 22:22:34 +0530

    Create pattern678.py

---
+++ pattern678.py
@@ -0,0 +1,15 @@
+#PATTERN --> 6
+n=4
+for i in range(1,n+1):
+    for j in range(1,n-i+2):
+        print("*",end="");
+    print()
+
+#PATTERN -->8
+n=6
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("*",end="");
+    print()
+
+#PATTERN -->7
